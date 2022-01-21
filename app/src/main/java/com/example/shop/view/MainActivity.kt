b2adf20329554f8c20b095897e781cda165af5c4
wo9374 <wo9374@naver.com>
2022-01-21 10:27:40 +0900

    안드로이드12 Splash 삭제

--- app/src/main/java/com/example/shop/view/MainActivity.kt
+++ app/src/main/java/com/example/shop/view/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.example.shop.view
 
-import android.animation.ObjectAnimator
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewTreeObserver
-import android.view.animation.AnticipateInterpolator
 import androidx.activity.viewModels
-import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -25,39 +18,12 @@ class MainActivity : AppCompatActivity() {
     //by viewModels() 사용 안할때
     //val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-    private var isReady = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
-
-        isReady = true
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f).run {
-                    interpolator = AnticipateInterpolator()
-                    duration = 200L
-                    doOnEnd { splashScreenView.remove() }
-                    start()
-                }
-            }
-        }
-
-        binding.root.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener{
-            override fun onPreDraw(): Boolean {
-                return if (isReady){
-                    binding.root.viewTreeObserver.removeOnPreDrawListener(this)
-                    true
-                }else{
-                    false
-                }
-            }
-        })
 
         val navHostFragment = supportFragmentManager.findFragmentById(binding.mainNavHost.id) as NavHostFragment
         val navController = navHostFragment.navController
